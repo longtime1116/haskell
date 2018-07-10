6bbf93195eb0ce57ce5283025bd1e9f79d0a82fa
longtime1116 <longtime1116@gmail.com>
2018-07-10 18:10:01 +0900

    ~1-4

--- baby.hs
+++ baby.hs
@@ -5,3 +5,10 @@ doubleSmallNumber x = if x > 100
                         else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 hoge = "Hoge"
+
+a = take 24 [13, 26..]
+b = take 10 (cycle [1,2,3])
+c = take 10 (repeat 5)
+d = replicate 3 10
+
+
