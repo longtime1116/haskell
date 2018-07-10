9f7bb74a94fc8bb9f74e4838dc0987722b6cd352
longtime1116 <longtime1116@gmail.com>
2018-07-10 19:06:05 +0900

    ~1-5

--- baby.hs
+++ baby.hs
@@ -6,9 +6,12 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 hoge = "Hoge"
 
+-- リストとか
 a = take 24 [13, 26..]
 b = take 10 (cycle [1,2,3])
 c = take 10 (repeat 5)
 d = replicate 3 10
-
+modBetween50and100 xs = [x | x <- [50..100], x `mod` xs == 3]
+funcA = [x * y | x <- [1, 2, 7], y <- [1, 2, 7]]
+length' xs = sum [1 | _ <- xs]
 
