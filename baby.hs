35fbdb9e0834310f8a092130f9ef67d8fa363cf3
shumo.tokinaga <longtime1116@gmail.com>
2018-07-12 12:07:54 +0900

    in 2.4

--- baby.hs
+++ baby.hs
@@ -26,8 +26,29 @@ b5 = zip [1, 2, 3, 4, 5] ["a", "b", "c"]
 -- 直角三角形
 b6 = take 10 [[c, b, a] | a <- [1..], b <- [1..a], c <- [1..b],
                           a^2 == b^2 + c^2]
-
 rightAngleTriangle x = take x [[c, b, a] | a <- [1..],
                                            b <- [1..a],
                                            c <- [1..b],
                                            a^2 == b^2 + c^2]
+
+-- product は 要素を全て掛け合わせる
+factorial :: Integer ->Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+-- :t head    # head :: [a] -> a
+
+-- :t (==)    # (==) :: Eq a => a -> a -> Bool
+
+-- :t (>)     # (>) :: Ord a => a -> a -> Bool
+-- "ABC" < "BCD"
+-- "ABC" `compare` "BCD"
+-- "BCD" `compare` "ABC"
+
+-- :t show    # show :: Show a => a -> String
+-- show True
+
