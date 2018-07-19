469af8c296fbef1cadcbf3775238edd362ec2d50
shumo.tokinaga <longtime1116@gmail.com>
2018-07-19 19:48:17 +0900

    ~4.2(not yet)

--- book.hs
+++ book.hs
@@ -182,3 +182,28 @@ describeList ls = "The list is " ++ what ls
   where what [] = "empty."
         what [x] = "a singleton list."
         what xs = "a longer list."
+
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "error!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n-1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' a = [a] ++ repeat' a
