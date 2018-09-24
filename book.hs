a89bcc6c2418154ceb48d60a9b72cd6edda91673
longtime1116 <longtime1116@gmail.com>
2018-09-24 17:41:48 +0900

    implement map and filter

--- book.hs
+++ book.hs
@@ -280,5 +280,13 @@ flip' f x y = f y x
 -- geho = flip' compare 100 2
 
 -- map
+map' :: (t -> a ) -> [t] -> [a]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
 
 -- filter
+filter' :: (a -> Bool ) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+  | f x = x : filter' f xs
+  | otherwise = filter' f xs
