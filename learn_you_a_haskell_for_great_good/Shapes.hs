067c2d71cb87b9e5f9364820930d6cfb87c938d6
shumo.tokinaga <longtime1116@gmail.com>
2018-09-26 18:04:29 +0900

    baseRect/baseCircle/nudge

--- learn_you_a_haskell_for_great_good/Shapes.hs
+++ learn_you_a_haskell_for_great_good/Shapes.hs
@@ -2,6 +2,9 @@ module Shapes
 ( Shape(..)
 , Point(..)
 , area
+, nudge
+, baseCircle
+, baseRectangle
 ) where
 
 data Point = Point Float Float deriving (Show)
@@ -11,3 +14,13 @@ area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b
+    = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRectangle :: Float -> Float -> Shape
+baseRectangle w h = Rectangle (Point 0 0) (Point w h)
