64d8cb3d13ed2127c4770076f4921deb4262cbe0
longtime1116 <longtime1116@gmail.com>
2018-09-27 22:57:27 +0900

    独自の型クラス

--- learn_you_a_haskell_for_great_good/book.hs
+++ learn_you_a_haskell_for_great_good/book.hs
@@ -502,3 +502,16 @@ lockers = Map.fromList
 
 numsForTree = [8,6,1,7,3,5]
 numsTree = foldr treeInsert EmptyTree numsForTree
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red! Stop!"
+    show Yellow = "Yellow! Go carefully!"
+    show Green = "Green! Go Go Go!"
