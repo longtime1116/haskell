bf3bb1bb292590830296508ea5950c887ac617ce
longtime1116 <longtime1116@gmail.com>
2018-09-29 23:21:54 +0900

    do in main

--- learn_you_a_haskell_for_great_good/hello.hs
+++ learn_you_a_haskell_for_great_good/hello.hs
@@ -1,2 +1,7 @@
 -- $ ghc --make hello
-main = putStrLn "hello, world"
+-- main = putStrLn "hello, world"
+
+main = do
+    putStrLn "Hello, what's your name?"
+    name <- getLine
+    putStrLn $ "Hello, " ++ name
