261cdb0c724d02eab8be67cacc345ec9e467aca2
longtime1116 <longtime1116@gmail.com>
2018-09-29 23:37:48 +0900

    I/O action

--- learn_you_a_haskell_for_great_good/hello.hs
+++ learn_you_a_haskell_for_great_good/hello.hs
@@ -1,7 +1,33 @@
 -- $ ghc --make hello
 -- main = putStrLn "hello, world"
 
+-- main = do
+--     putStrLn "Hello, what's your name?"
+--     name <- getLine
+--     putStrLn $ "Hello, " ++ name
+
+
+-- let は pure な式に名前を束縛するときに使う
+-- import Data.Char
+--
+-- main = do
+--     putStrLn "What's your first name?"
+--     firstName <- getLine
+--     putStrLn "What's your last name?"
+--     lastName <- getLine
+--     let bigFirstName = map toUpper firstName
+--         bigLastName = map toUpper lastName
+--     putStrLn $ "Hello, " ++ bigFirstName ++ " " ++ bigLastName ++ "."
+
+
+-- return () は引数で渡した pure な値から I/O アクションを作り出すもの
 main = do
-    putStrLn "Hello, what's your name?"
+    putStrLn "What's your name?"
     name <- getLine
-    putStrLn $ "Hello, " ++ name
+    if null name
+        then return ()
+        else do
+            putStrLn $ reverseWords name
+            main
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
